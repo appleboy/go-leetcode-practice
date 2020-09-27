34c96693c76c23e6f1d89587689025f097bb75bc
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-09-27 10:20:50 +0800

    chore(278): update
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- easy/first-bad-version/solution.go
+++ easy/first-bad-version/solution.go
@@ -1,3 +1,7 @@
+package main
+
+import "fmt"
+
 /**
  * Forward declaration of isBadVersion API.
  * @param   version   your guess about first bad version
@@ -7,19 +11,34 @@
  */
 
 func search(start, end int) int {
-		half := int((end - start)/2) + start
-		if !isBadVersion(half) {
-				return search(half+1, end)
-		}
+	half := int((end-start)/2) + start
+	if !isBadVersion(half) {
+		return search(half+1, end)
+	}
 
-		if isBadVersion(half-1) {
-				return search(start, half)
-		}
+	if isBadVersion(half - 1) {
+		return search(start, half)
+	}
 
-		return half
+	return half
 
 }
 
+func isBadVersion(v int) bool {
+	switch v {
+	case 8, 9, 10:
+		return true
+	default:
+		return false
+	}
+
+	return false
+}
+
 func firstBadVersion(n int) int {
-		return search(1, n+1)
+	return search(1, n+1)
+}
+
+func main() {
+	fmt.Println(firstBadVersion(10))
 }
